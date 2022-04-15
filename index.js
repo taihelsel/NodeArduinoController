3ee7573ad360514210e207ce940a4b3ec2fc12f4
taihelsel <tai.bayard@gmail.com>
2022-04-15 01:42:27 +0000

    passing command as args. added exit logic to node

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 const { SerialPort } = require('serialport')
 const { ReadlineParser } = require('@serialport/parser-readline')
-
+//command to send to controller
+const command = process.argv[2];
+//connect to serial port
 const port = new SerialPort({ path: '/dev/ttyACM0', baudRate: 9600 })
+//initialize parser
 const parser = new ReadlineParser()
 port.pipe(parser)
+//wait for controller to ready
 let ready = false;
-parser.on('data', (txt)=>{
-	if(txt.indexOf("Ready to send IR signals at pin 3")!==-1){
-		ready=true;
+parser.on('data', (txt) => {
+	if (txt.indexOf("Ready to send IR signals at pin 3") !== -1) {
+		ready = true;
 	}
-	console.log(txt);
+	console.log(txt); //read controller response
 })
-const readyWait = setInterval(function(){
-	if(ready===false){
+const readyWait = setInterval(function () {
+	if (ready === false) {
 		console.log("Waiting for controller");
 		port.write("Waiting..\n");
-	}else{
+	} else {
 		clearInterval(readyWait);
-		port.write("Power\n");
+		executeCommand();
 	}
-},1000);
+}, 1000);
+
+const executeCommand = function () {
+	port.write(`${command}\n`);
+	console.log(`Sent ${command} command\r\nExiting Node`);
+	process.exit(1);
+}
