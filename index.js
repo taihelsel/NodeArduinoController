38d584c1fd33f677ac4dbd27aee868b632bab8fd
taihelsel <tai.bayard@gmail.com>
2022-04-15 02:12:15 +0000

    added variable temp controls and case checking in command execution

--- index.js
+++ index.js
@@ -25,8 +25,39 @@ const readyWait = setInterval(function () {
 	}
 }, 1000);
 
-const executeCommand = function () {
-	port.write(`${command}\n`);
-	console.log(`Sent ${command} command\r\nExiting Node`);
+function tempChangeByX(args, direction) {
+	console.log(args);
+	let x = parseInt(args[1]);
+	if (isNaN(x)) {
+		console.log("Error getting temp value");
+		exit();
+	} else {
+		const tempLoop = setInterval(function () {
+			if (x === 0) {
+				clearInterval(tempLoop);
+				console.log("Done updating temp");
+				exit();
+			} else {
+				x--;
+				port.write(`Temp${direction}\n`);
+			}
+		}, 500);
+	}
+}
+function executeCommand() {
+	const lower = command.toLowerCase();
+	if (lower.indexOf("temp+") !== -1) tempChangeByX(command.split("+"), "Up");
+	else if (lower.indexOf("temp-") !== -1) tempChangeByX(command.split("-"), "Down");
+	else {
+		if (lower === "tempup") port.write("TempUp\n");
+		else if (lower === "tempdown") port.write("TempDown\n");
+		else { //if future commands are written, they wont be case checked but can still be executed
+			port.write(`${command}\n`);
+		}
+		exit();
+	}
+}
+function exit() {
+	console.log("Exiting Node");
 	process.exit(1);
 }
