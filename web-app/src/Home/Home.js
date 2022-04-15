a6e0dd6697debe18788cd8b4af7015fe013a4f49
taihelsel <tai.bayard@gmail.com>
2022-04-15 18:58:51 +0000

    added 5deg shortcuts

--- web-app/src/Home/Home.js
+++ web-app/src/Home/Home.js
@@ -1,14 +1,22 @@
 import "./Home.css";
 import PowerBtn from "../Components/PowerBtn/PowerBtn";
 import TempSlider from "../Components/TempSlider/TempSlider";
+import Temp5deg from "../Components/Temp5deg/Temp5deg";
 function Home() {
     const powerClick = () => {
+        alert("working");
+    }
+    const handle5degClick = direction => e => {
         alert("working");
     }
     return (
         <section id="Home">
             <PowerBtn handlePower={powerClick} />
             <TempSlider />
+            <div className="home-btns-row">
+                <Temp5deg handleClick={handle5degClick} direction={"cold"} />
+                <Temp5deg handleClick={handle5degClick} direction={"hot"} />
+            </div>
         </section>
     );
 }
