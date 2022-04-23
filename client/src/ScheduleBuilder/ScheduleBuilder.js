5e26a743b20a2f5838fa7e77e7c25928021a7e9b
taihelsel <tai.bayard@gmail.com>
2022-04-23 21:25:17 +0000

    added first test post logic for sending a scheduledEvent to scheduler

--- client/src/ScheduleBuilder/ScheduleBuilder.js
+++ client/src/ScheduleBuilder/ScheduleBuilder.js
@@ -7,10 +7,10 @@ function ScheduleBuilder({ updatePage }) {
     const [schedInput, setSchedInput] = useState("");
     const [nextAction, setNextAction] = useState({ showNext: false, nextStep: null });
     const [schedConfig, setSchedConfig] = useState({
-        exeTime: null,
+        exeTime: 1234,
         command: null,
-        reoccuring: null,
-        interval: null, //min to add each time new schedule is added
+        reoccuring: true,
+        interval: 1, //min to add each time new schedule is added
         desc: {
             every: null,
             task: null,
@@ -20,14 +20,36 @@ function ScheduleBuilder({ updatePage }) {
     const handleCancel = () => {
         updatePage("schedule")
     }
+    const submitScheduledEvent = () => {
+        const url = "/schedule/";
+        const data = { "exeTime": schedConfig.exeTime, "scheduledEvent": schedConfig };
+        fetch(url, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json",
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                updatePage("schedule");
+                alert(data.msg);
+            });
+    }
     const handleNext = () => {
         if (nextAction.showNext === true) {
-            setCurrentStep(nextAction.nextStep);
-            setNextAction({
-                showNext: false,
-                nextStep: null,
-            });
-            setSchedInput("");
+            if (nextAction.nextStep === "save") {
+                //submit scheduled event and go back to schedule list
+                submitScheduledEvent();
+            } else {
+                setCurrentStep(nextAction.nextStep);
+                setNextAction({
+                    showNext: false,
+                    nextStep: null,
+                });
+                setSchedInput("");
+                setSelectedStepOption("");
+            }
         }
     }
     const handleOptionClick = (nextStep, option) => e => {
@@ -45,6 +67,19 @@ function ScheduleBuilder({ updatePage }) {
             if (option === "am" || option === "pm") {
                 const newSchedConfig = { ...schedConfig };
                 newSchedConfig.desc.every[1] = schedInput + option;
+                setSchedConfig(newSchedConfig);
+                setNextAction({
+                    showNext: true,
+                    nextStep,
+                });
+            }
+        }
+        if (nextStep === "save") {
+            if (option === "power") {
+                const newSchedConfig = { ...schedConfig };
+                newSchedConfig.desc.task = "toggle";
+                newSchedConfig.desc.command = "Power";
+                newSchedConfig.command = "Power";
                 setSchedConfig(newSchedConfig);
                 setNextAction({
                     showNext: true,
@@ -85,10 +120,17 @@ function ScheduleBuilder({ updatePage }) {
                 </div>
             )
         }
+
         if (step === "set-task") {
             return (
                 <div className="sched-step-container">
-                    {JSON.stringify(schedConfig)}
+                    <h1 className="sched-step-title">Set Schedule</h1>
+                    <div className="sched-step-body-row">
+                        <div onClick={handleOptionClick("temp-inc-input", "inc-temp-x")} id={selectedStepOption === "inc-temp-x" ? "sched-step-option-selected" : null} className="sched-step-option">Increase Temp By X</div>
+                        <div onClick={handleOptionClick("temp-target-input", "set-temp")} id={selectedStepOption === "set-temp" ? "sched-step-option-selected" : null} className="sched-step-option">Set Temp To X</div>
+                        <div onClick={handleOptionClick("save", "power")} id={selectedStepOption === "power" ? "sched-step-option-selected" : null} className="sched-step-option">Toggle Power</div>
+                        <div onClick={handleOptionClick("temp-dec-input", "dec-temp-x")} id={selectedStepOption === "dec-temp-x" ? "sched-step-option-selected" : null} className="sched-step-option">Decrease Temp By X</div>
+                    </div>
                 </div>
             )
         }
@@ -100,7 +142,7 @@ function ScheduleBuilder({ updatePage }) {
             </div>
             <div className="sched-builder-controls-container">
                 <div className="sched-builder-controls sched-builder-cancel" onClick={handleCancel}><h3>Cancel</h3></div>
-                <div className={`sched-builder-controls sched-builder-confirm ${nextAction.showNext === false ? "sched-builder-confirm-disabled" : null}`} onClick={handleNext}><h3>Next</h3></div>
+                <div className={`sched-builder-controls sched-builder-confirm ${nextAction.showNext === false ? "sched-builder-confirm-disabled" : null}`} onClick={handleNext}><h3>{nextAction.nextStep === "save" ? "Save" : "Next"}</h3></div>
             </div>
         </section>
     );
